6e78c4ebb043c1861f6fe35516de7fc80e7e7f93
Jeff Ward <jeffwardcoding@gmail.com>
2015-11-07 19:48:22 -0800

    working presentation files

---
+++ js/pageFlow.js
@@ -0,0 +1,87 @@
+//Shrinks Search Bar to small size on load
+$(document).ready(function () {
+
+    //Hide Table 
+    $('.tableRow').hide();
+    $('#ClearBtn').hide();
+
+    var searchField = $('#SearchInput');
+    var icon = $('.SearchBtn');
+
+    $(searchField).animate({
+        width: '45%'
+    }, 400, function () {}); //End searchField Animate
+
+    $(icon).animate({
+        right: '270px'
+    }, 400, function () {}); //End icon animate 
+});
+
+//Changes size of Search Bar on focus
+$(function () {
+    var searchField = $('#SearchInput');
+    var icon = $('.SearchBtn');
+
+
+    //focus event handler
+    $(searchField).on('focus', function () {
+        $(this).animate({
+            width: '100%'
+        }, 400); //End Search Field Animate
+
+        $(icon).animate({
+            right: '10px'
+        }, 400); //end icon animate
+    }); //End Focus Handler
+
+
+    //Blur event handler
+    $(searchField).on('blur', function () {
+        if (searchField.val() == '') {
+            $(searchField).animate({
+                width: '45%'
+            }, 400, function () {}); //End searchField Animate
+
+            $(icon).animate({
+                right: '270px'
+            }, 400, function () {}); //End icon animate
+        } //End If statement
+    }); //End Focus Handler
+
+
+}); //End Document Ready
+
+//Show Table on click
+$('#GoBtn').on('click', function () {
+    
+    
+  
+    
+    $('#ClearBtn').show();
+})
+
+//ClearButton
+$('#ClearBtn').on('click', function () {
+
+    //Clear Divs
+    $('.tableRow').hide();
+    $('#SearchLocation').text("");
+    $('#subHeading').text("");
+    $('#CurrentWeather').text("");
+    $('#Temperature').text("");
+    $('#FeelsLike').text("");
+    $('#longitude').text("");
+    $('#latitude').text("")
+    $('#elevation').text("")
+    $('#timezone').text("");
+    $('#uv').text("");
+    $('#humidity').text("");
+    $('#windStatus').text("");
+    $('#windSpeed').text("");
+    $('#windDirection').text("");
+    $('#iconImage').empty();
+    $('#ClearBtn').hide();
+    $('#SearchInput').val("");
+
+})
+
